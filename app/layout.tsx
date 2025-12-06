9736552e20089ad1f4d39530ede2ad742adee1b2
Marcelo Fuentes <marceloe.fuentes@gmail.com>
2025-12-06 01:11:39 -0300

    chore: include vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { PlaylistDraftProvider } from "@/contexts/playlist-draft-context"
@@ -66,6 +67,7 @@ export default function RootLayout({
             </PlaylistsProvider>
           </LocaleProvider>
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   )
