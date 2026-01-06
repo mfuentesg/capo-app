468f6370a8df41b4bc359d17502f27281f5dcc51
Marcelo Fuentes <marceloe.fuentes@gmail.com>
2026-01-06 01:05:41 -0300

    chore: clean up next.config.ts by removing unnecessary semicolons and adjusting formatting

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   async headers() {
@@ -23,8 +23,8 @@ const nextConfig: NextConfig = {
           }
         ]
       }
-    ];
+    ]
   }
-};
+}
 
-export default nextConfig;
+export default nextConfig
