9232ed1715f84671e3bde6e85b1e2226784a1360
Marcelo Fuentes <marceloe.fuentes@gmail.com>
2026-02-12 04:12:35 -0300

    chore: remove temporary SQL snippet file

--- supabase/snippets/Untitled query 456.sql
+++
@@ -1,7 +0,0 @@
-SELECT invite_team_member(
-  'f82b7d5c-442b-4fb5-b700-df089ef4e26e'::uuid,
-  'me@mfuentesg.dev',
-  'member'::team_role_enum
-);
-
-SELECT * FROM net._http_response ORDER BY created DESC LIMIT 10;
