35cfc4ef6b08e4f9f2f9ef0c3beb323236d74e6a
Marcelo Fuentes <marceloe.fuentes@gmail.com>
2026-02-13 00:00:04 -0300

    Guard pg_net schema move

--- supabase/migrations/20260213024700_move_pg_net_schema.sql
+++ supabase/migrations/20260213024700_move_pg_net_schema.sql
@@ -8,9 +8,19 @@ BEGIN
 			JOIN pg_namespace nsp ON nsp.oid = ext.extnamespace
 			WHERE ext.extname = 'pg_net'
 				AND nsp.nspname <> 'extensions'
+				AND ext.extrelocatable
 		) THEN
 			CREATE SCHEMA IF NOT EXISTS extensions;
 			ALTER EXTENSION pg_net SET SCHEMA extensions;
+		ELSIF EXISTS (
+			SELECT 1
+			FROM pg_extension ext
+			JOIN pg_namespace nsp ON nsp.oid = ext.extnamespace
+			WHERE ext.extname = 'pg_net'
+				AND nsp.nspname <> 'extensions'
+				AND NOT ext.extrelocatable
+		) THEN
+			RAISE NOTICE 'pg_net is not relocatable; skipping schema move.';
 		END IF;
 	END IF;
 END $$;
